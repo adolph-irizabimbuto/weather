0be6c99e2c8b34c1a63b4a140b01fbd9ca861723
“azulaZen” <adolph_12@hotmail.com.au~>
2022-02-27 16:03:26 +0800

    Updates

--- weatherScript.js
+++ weatherScript.js
@@ -1,56 +1,19 @@
 window.addEventListener("load", ()=>{
-    let long;
-    let lat;
-    //initialising DOM elements
-    
-  
-    //native weather 
-    const tempDegree = document.querySelector('.temperature-degree');
-    const temperatureDescription = document.querySelector('.temperature-description');
-    const location = document.querySelector('.location-timezone');
-    const weatherIcon = document.querySelector('.weather-icon');
-    const tempFeren = document.querySelector('.temperature-degree-feren');
-    const dateTime = document.querySelector('.date-time');
-
-    //Paris
-    
-    const paris_tempDegree = document.querySelector('.temperature-degree-paris');
-    const paris_temperatureDescription = document.querySelector('.temperature-description-paris');
-    const paris_location = document.querySelector('.location-timezone-paris');
-    const paris_weatherIcon = document.querySelector('.weather-icon-paris');
-    const paris_tempFeren = document.querySelector('.temperature-degree-feren-paris');
-    const paris_dateTime = document.querySelector('.date-time-paris');
-    
-    //LA
-    
-    const la_tempDegree = document.querySelector('.temperature-degree-la');
-    const la_temperatureDescription = document.querySelector('.temperature-description-la');
-    const la_location = document.querySelector('.location-timezone-la');
-    const la_weatherIcon = document.querySelector('.weather-icon-la');
-    const la_tempFeren = document.querySelector('.temperature-degree-feren-la');
-    const la_dateTime = document.querySelector('.date-time-la');
-    //Sydney 
-    
-    const sydney_tempDegree = document.querySelector('.temperature-degree-sydney');
-    const sydney_temperatureDescription = document.querySelector('.temperature-description-sydney');
-    const sydney_location = document.querySelector('.location-timezone-sydney');
-    const sydney_weatherIcon = document.querySelector('.weather-icon-sydney');
-    const sydney_tempFeren = document.querySelector('.temperature-degree-feren-sydney');
-    const sydney_dateTime = document.querySelector('.date-time-sydney');
-    
+   
     //Date 
     const options = {hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true };
     let date = new Date().toDateString();
-    let time =new Date().toLocaleDateString('en-US', options).slice(-11)
+    // the slice is a cheap hack to obtain the following format "hh:mm:ss"
     
+    let time =new Date().toLocaleDateString('en-US', options).slice(-11)
+   
+  
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position =>{
-            
-            long = position.coords.longitude;
-            lat = position.coords.latitude;  
-
             //coordinates
             let coordinates = {
+                nativeLong: position.coords.longitude,
+                nativeLat: position.coords.latitude,
                 parisLat:48.8534, 
                 parisLong: 2.3488,
                 laLat:34.0522, 
@@ -58,133 +21,69 @@ window.addEventListener("load", ()=>{
                 sydneyLat: 35.6895,
                 sydneyLong: 139.6917,
             }
-            const wheatherDBArray= [];
-            //API calls
 
+        const logData = async ()=>{
+            
+            // add to nect 
+            // time and date
+            // const date_and_time = {
+            //     native: `${date}/${time}`,
+            //     paris: convertTimeZOne(new Date(), "Europe/Paris"),
+            //     la: convertTimeZOne(new Date(), "America/Los_Angeles"),
+            //     sydney: `${date}/${time}`,
+            // }
+      
+            // array to save fetch responses
+            let newDB =[];
+            // initialising varible that will be used to itirate through newDB array
+            let i =0;
+            // object URL's to be fetched
             const apis ={
-                native:`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=5b37ba235354d2d3a601644e092a984b&units=metric `,
-                paris: `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=${coordinates['parisLat']}&lon=${coordinates['parisLong']}&appid=5b37ba235354d2d3a601644e092a984b&units=metric `,
-                la: `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=${coordinates['laLat']}&lon=${coordinates['laLong']}&appid=5b37ba235354d2d3a601644e092a984b&units=metric `,
-                sydney: `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=${coordinates['sydneyLat']}&lon=${coordinates['sydneyLong']}&appid=5b37ba235354d2d3a601644e092a984b&units=metric `
-
+                native: `http://api.openweathermap.org/data/2.5/weather?lat=${coordinates['nativeLat']}&lon=${coordinates['nativeLong']}&appid=5b37ba235354d2d3a601644e092a984b&units=metric `,
+                paris: `http://api.openweathermap.org/data/2.5/weather?lat=${coordinates['parisLat']}&lon=${coordinates['parisLong']}&appid=5b37ba235354d2d3a601644e092a984b&units=metric `,
+                la: `http://api.openweathermap.org/data/2.5/weather?lat=${coordinates['laLat']}&lon=${coordinates['laLong']}&appid=5b37ba235354d2d3a601644e092a984b&units=metric `,
+                sydney: `http://api.openweathermap.org/data/2.5/weather?lat=${coordinates['sydneyLat']}&lon=${coordinates['sydneyLong']}&appid=5b37ba235354d2d3a601644e092a984b&units=metric `
+        
             }
+            //fetch data from Open Weather Map servers then parse to JSON
+            const native = fetch(apis.native).then(value => value.json())
+            const paris = fetch(apis.paris).then(value => value.json())
+            const la =  fetch(apis.la).then(value => value.json())
+            const sydney = fetch(apis.sydney).then(value => value.json())
 
-            //this adds each response to weatherDB array 
-           console.log(wheatherDBArray.length)
-           console.log(wheatherDBArray)
-           console.log('i need to get head during bhm')
-             
-           Object.values(apis).forEach(values =>{
-                fetch(values)
-                    .then(res =>{
-                        return res.json();
-                    })
-                    .then(newData =>{
-                        wheatherDBArray.push(newData)
-                    })
-                    
-            })
-            console.log(wheatherDBArray.length)
-           
-            const native = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=5b37ba235354d2d3a601644e092a984b&units=metric `
-            const paris = `http://api.openweathermap.org/data/2.5/weather?lat=${coordinates['parisLat']}&lon=${coordinates['parisLong']}&appid=5b37ba235354d2d3a601644e092a984b&units=metric `
-            const la = `http://api.openweathermap.org/data/2.5/weather?lat=${coordinates['laLat']}&lon=${coordinates['laLong']}&appid=5b37ba235354d2d3a601644e092a984b&units=metric `
-            const sydney=  `http://api.openweathermap.org/data/2.5/weather?lat=${coordinates['sydneyLat']}&lon=${coordinates['sydneyLong']}&appid=5b37ba235354d2d3a601644e092a984b&units=metric `
-
-
-            fetch(native)
-                .then(res =>{
-                    return res.json();
+            //retuns a promise ONCE all the iterable fetch api's have been resolved
+            const data = await Promise.all([native, paris, la, sydney])
+            newDB.push(data);
+                //iterates though each KVP in the "apis" oject then uses the key to paint the DOM with the content associated with that key.
+                Object.keys(apis).forEach(key => {
+                    document.querySelector(`.temperature-degree-${key}`).textContent = Math.floor(newDB[0][i].main.temp);
+                    document.querySelector(`.temperature-description-${key}`).textContent = `you can expect ${newDB[0][i].weather[0].description}`.toUpperCase();
+                    document.querySelector(`.location-timezone-${key}`).textContent = `${newDB[0][i].name}, ${newDB[0][i].sys.country}` ;
+                    document.querySelector(`.weather-icon-${key}`).textContent = newDB[0][i].weather[0].icon;
+                    document.querySelector(`.temperature-degree-feren-${key}`).textContent = Math.floor(celciusToFerenheight(newDB[0][i].main.temp));
+                    document.querySelector(`.date-time-${key}`).textContent = `${date} | ${time}`
+                    //incriments i
+                    i++
                 })
-                .then(data =>{
-                    //extracting data from the response object 
-                    const{temp,temp_max,temp_min,humidity,pressure}= data.main;
-                    const suburb = data.name;
-                    const country = data.sys.country;
-                    const {description,icon,}= data.weather[0];
-                    const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`
-        
-                    //interractions with DOM elements 
-                    tempDegree.textContent = Math.floor(temp);
-                    tempFeren.textContent = Math.floor(celciusToFerenheight(temp));
-                    location.textContent = `${suburb}, ${country}`;
-                    temperatureDescription.textContent = `you can expect ${description}`.toUpperCase();
-                    weatherIcon.setAttribute('src', iconUrl);
-                    dateTime.textContent = `${date} | ${time}`
-                });
-
-            fetch(paris)
-                .then(res =>{
-                    return res.json();
-                })
-                .then(data =>{
-                    //extracting data from the response object 
-                    const{temp,temp_max,temp_min,humidity,pressure}= data.main;
-                    const suburb = data.name;
-                    const country = data.sys.country;
-                    const {description,icon,}= data.weather[0];
-                    const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`
-        
-                    //interractions with DOM elements 
-                    paris_tempDegree.textContent = Math.floor(temp);
-                    paris_tempFeren.textContent = Math.floor(celciusToFerenheight(temp));
-                    paris_location.textContent = `${suburb}, ${country}`;
-                    paris_temperatureDescription.textContent = `you can expect ${description}`.toUpperCase();
-                    paris_weatherIcon.setAttribute('src', iconUrl);
-                    paris_dateTime.textContent = `${date} | ${time}`
-                })
-
-                fetch(la)
-                .then(res =>{
-                    return res.json();
-                })
-                .then(data =>{
-                    //extracting data from the response object 
-                    const{temp,temp_max,temp_min,humidity,pressure}= data.main;
-                    const suburb = data.name;
-                    const country = data.sys.country;
-                    const {description,icon,}= data.weather[0];
-                    const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`
-        
-                    //interractions with DOM elements 
-                    la_tempDegree.textContent = Math.floor(temp);
-                    la_tempFeren.textContent = Math.floor(celciusToFerenheight(temp));
-                    la_location.textContent = `${suburb}, ${country}`;
-                    la_temperatureDescription.textContent = `you can expect ${description}`.toUpperCase();
-                    la_weatherIcon.setAttribute('src', iconUrl);
-                    la_dateTime.textContent = `${date} | ${time}`
-                })
-
-                fetch(sydney)
-                .then(res =>{
-                    return res.json();
-                })
-                .then(data =>{
-                    //extracting data from the response object 
-                    const{temp,temp_max,temp_min,humidity,pressure}= data.main;
-                    const suburb = data.name;
-                    const country = data.sys.country;
-                    const {description,icon,}= data.weather[0];
-                    const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`
-        
-                    //interractions with DOM elements 
-                    sydney_tempDegree.textContent = Math.floor(temp);
-                    sydney_tempFeren.textContent = Math.floor(celciusToFerenheight(temp));
-                    sydney_location.textContent = `${suburb}, ${country}`;
-                    sydney_temperatureDescription.textContent = `you can expect ${description}`.toUpperCase();
-                    sydney_weatherIcon.setAttribute('src', iconUrl);
-                    sydney_dateTime.textContent = `${date} | ${time}`
-                })
+            
+        }
+        logData()
+       
         });  
         }  
         // handle an absent geolocation
     else{
        temperatureDescription.textContent = "Please allow location services for this to work"
     }
-
     function celciusToFerenheight(celcius){
         return (celcius*1.8)+ 32
     }
 
 
+    // add to next update
+    // function convertTimeZOne(date, tzString) {
+    //     return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {timeZone: tzString}));   
+    // // }
+    //         const europe = convertTimeZOne(new Date(), "Europe/Paris");
+    //         const toString = europe.toString()
 });
